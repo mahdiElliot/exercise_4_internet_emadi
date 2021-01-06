cd920449bed8c632230a5dc1353adb82b9a4924e
mahdij <mahdimn2011@yahoo.com>
2021-01-06 11:42:10 +0330

    init

---
+++ src/main/java/com/example/exercise_4_internet/Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise_4_internet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
